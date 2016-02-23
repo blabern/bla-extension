2560b28f17ee2ed7e3b8106e726be37aabda76a6
Oleg Slobodskoi <oleg008@gmail.com>
2016-02-23 02:40:37 +0100

    working ext

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
+(function() {
 
-function getSubtitle() {
-  var spans = document.querySelectorAll('.player-timedtext span')
-  return [].slice.call(spans).map(function(span) {
+var containerClass = 'player-timedtext-text-container'
+var baseUrl = 'https://bla-server.herokuapp.com/subtitle'
+
+function toArray(obj) {
+  return [].slice.call(obj)
+}
+
+function getSubtitle(container) {
+  var spans = container.querySelectorAll('span')
+  return toArray(spans).map(function(span) {
     return span.textContent
   }).join('\n')
 }
-
-function observe() {
-  var subtitles = document.querySelectorAll('.player-timedtext')[0]
-  var last
-  function changed() {
-    var text = getSubtitle()
-    if (!text || text === last) return
-    last = text
-    send(text, console.log.bind(console))
-  }
-
-  var observer = new MutationObserver(changed)
-  observer.observe(subtitles, {childList: true})
-}
-
-var baseUrl = 'https://bla-server.herokuapp.com/subtitle'
 
 function send(text, callback) {
   var url = baseUrl + '/' + encodeURI(text)
@@ -36,4 +28,19 @@ function send(text, callback) {
     })
 }
 
-observe()
+var onChange = (function() {
+  var last
+
+  return function(e) {
+    var node = e.target
+    if (!node.classList || !node.classList.contains(containerClass)) return
+    var text = getSubtitle(node)
+    if (!text || text === last) return
+    last = text
+    send(text, console.log.bind(console))
+  }
+}())
+
+document.addEventListener('DOMNodeInserted', onChange, false)
+}())
+
