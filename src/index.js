73a577fa05b891c9b112cd37024cf29467f07561
Oleg Slobodskoi <oleg008@gmail.com>
2016-02-27 11:44:40 +0100

    better parsing

--- src/index.js
+++ src/index.js
@@ -7,11 +7,16 @@ function toArray(obj) {
   return [].slice.call(obj)
 }
 
+/**
+ * Netflix newlines are split into divs with containerClass.
+ * We better preserve new lines.
+ */
 function getSubtitle(container) {
-  var spans = container.querySelectorAll('span')
-  return toArray(spans).map(function(span) {
-    return span.textContent
-  }).join('\n')
+  return toArray(container.querySelectorAll('.' + containerClass))
+    .map(function(node) {
+      return node.textContent
+    })
+    .join('\n')
 }
 
 function send(text, callback) {
@@ -31,16 +36,26 @@ function send(text, callback) {
 var onChange = (function() {
   var last
 
-  return function(e) {
-    var node = e.target
-    if (!node.classList || !node.classList.contains(containerClass)) return
+  function handle(node) {
     var text = getSubtitle(node)
+
     if (!text || text === last) return
     last = text
     send(text, console.log.bind(console))
   }
+
+  return function(e) {
+    var node = e.target
+    if (!node.classList || !node.classList.contains(containerClass)) return
+    var parent = node.parentNode
+    setTimeout(function() {
+      handle(parent)
+    }, 20)
+  }
 }())
 
-document.addEventListener('DOMNodeInserted', onChange, false)
+// DOM Mutation events are deprecated
+// https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Mutation_events
+document.addEventListener('DOMNodeInserted', onChange, true)
 }())
 
