9afa4b30d265e42af8a409aaf95110e63cec281e
Oleg Slobodskoi <oleg008@gmail.com>
2016-05-30 10:53:25 +0200

    added amazon video

--- src/js/content.js
+++ src/js/content.js
@@ -2,27 +2,26 @@
 
 var log = console.log.bind(console)
 
-var containerClass = 'player-timedtext-text-container'
-
 function toArray(obj) {
   return [].slice.call(obj)
 }
 
+function noop() {}
+
 /**
- * Netflix newlines are split into divs with containerClass.
- * We better preserve new lines.
+ * Compare a string with the previous version.
  */
-function getSubtitle(container) {
-  return toArray(container.querySelectorAll('.' + containerClass))
-    .map(function(node) {
-      return toArray(node.querySelectorAll('span'))
-        .map(function(node) {
-          return node.textContent
-        })
-        .join('\n')
-    })
-    .join('\n')
-}
+var hasChanged = (function() {
+  var previous
+
+  return function hasChanged(next) {
+    if (next !== previous) {
+      previous = next
+      return true
+    }
+    return false
+  }
+}())
 
 function send(subtitle, callback) {
   var message = {
@@ -34,15 +33,33 @@ function send(subtitle, callback) {
   chrome.runtime.sendMessage(message, callback)
 }
 
-var onChange = (function() {
-  var last
+var adapters = {}
+
+adapters.netflix = (function() {
+  var containerClass = 'player-timedtext-text-container'
+
+  /**
+   * Netflix newlines are split into divs with containerClass.
+   * We better preserve new lines.
+   */
+  function getSubtitle(container) {
+    return toArray(container.querySelectorAll('.' + containerClass))
+      .map(function(node) {
+        return toArray(node.querySelectorAll('span'))
+          .map(function(node) {
+            return node.textContent
+          })
+          .join('\n')
+      })
+      .join('\n')
+  }
 
   function handle(node) {
     var text = getSubtitle(node)
 
-    if (!text || text === last) return
-    last = text
-    send(text, console.log.bind(console))
+    if (text && hasChanged(text)) {
+      send(text, console.log.bind(console))
+    }
   }
 
   return function(e) {
@@ -55,9 +72,38 @@ var onChange = (function() {
   }
 }())
 
+adapters.amazon = (function() {
+  var containerClass = 'timedTextBackground'
+
+  function handle(node) {
+    var text = node.innerHTML.split('<br>').join('\n')
+    if (text && hasChanged(text)) {
+      send(text, console.log.bind(console))
+    }
+  }
+
+  return function(e) {
+    var node = e.target.parentNode
+    if (!node.classList || !node.classList.contains(containerClass)) return
+
+    setTimeout(function() {
+      handle(node)
+    }, 20)
+  }
+}())
+
+function getAdapter() {
+  for (var name in adapters) {
+    if (location.hostname.indexOf(name) !== -1) {
+      return adapters[name]
+    }
+  }
+  return noop
+}
+
 // DOM Mutation events are deprecated
 // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Mutation_events
-document.addEventListener('DOMNodeInserted', onChange, true)
+document.addEventListener('DOMNodeInserted', getAdapter(), true)
 
 }())
 
