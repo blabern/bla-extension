dd095d64e2b87e0a09facb6468c9a355c6f92357
Oleg Isonen <oleg008@gmail.com>
2021-01-01 17:26:26 +0100

    render connection status when extension loads

--- src/js/popup.js
+++ src/js/popup.js
@@ -29,6 +29,42 @@
         });
     }
 
+    function fetchConnected(auth, callback) {
+      var options = {
+        method: "POST",
+        body: JSON.stringify({ auth }),
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+      };
+      fetch(baseUrl + "/connected", options)
+        .then(function (res) {
+          return res.json();
+        })
+        .then(callback)
+        .catch(function (err) {
+          callback({ error: err.message, stack: err.stack });
+        });
+    }
+
+    function renderStatus(res) {
+      statusEl.classList.remove("hidden");
+      if (res.error) {
+        statusEl.classList.add("error");
+        statusEl.textContent = res.error;
+        return;
+      }
+
+      if (res.connected === 0) {
+        statusEl.classList.add("error");
+        statusEl.textContent = "No Web App is connected";
+        return;
+      }
+
+      statusEl.classList.remove("error");
+      statusEl.textContent = res.connected + " Web Apps are connected";
+    }
+
     function setEmail() {
       var email = emailEl.value.trim();
       var message = {
@@ -39,23 +75,7 @@
       chrome.runtime.sendMessage(message, function (res) {
         console.log(res);
 
-        sendTestSubtitle(email, function (res) {
-          statusEl.classList.remove("hidden");
-          if (res.error) {
-            statusEl.classList.add("error");
-            statusEl.textContent = res.error;
-            return;
-          }
-
-          if (res.connected === 0) {
-            statusEl.classList.add("error");
-            statusEl.textContent = "No Web App is connected";
-            return;
-          }
-
-          statusEl.classList.remove("error");
-          statusEl.textContent = res.connected + " Web Apps are connected";
-        });
+        sendTestSubtitle(email, renderStatus);
       });
     }
 
@@ -67,9 +87,14 @@
       function (res) {
         if (res.email) {
           emailEl.value = res.email;
+          fetchConnected(res.email, renderStatus);
         }
       }
     );
+
+    emailEl.addEventListener("keypress", () => {
+      statusEl.classList.add("hidden");
+    });
 
     emailFormEl.addEventListener("submit", (event) => {
       event.preventDefault();
